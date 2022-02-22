a1059c3bcb4237e57da1a3b8c050e91638b9bbcb
Luca Oberhofer <luca.oberhofer@burda-forward.de>
2022-02-22 22:38:20 +0100

    Tailwind works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
